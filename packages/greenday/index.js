c67edf8fb76f65f31ef61646a9ab88c8d43c9b9a
Shintaro-Kurihara-Ktc <115063148+Shintaro-Kurihara-Ktc@users.noreply.github.com>
2025-04-07 14:12:22 +0900

    fix: hotfix (#27)

--- packages/greenday/index.js
+++ packages/greenday/index.js
@@ -8,6 +8,7 @@ console.log('aaaaaaaac');
 console.log('aaaaaaaac');
 console.log('aaaaaaaac');
 console.log('aaaaaaaac');
+console.log('aaaaaaaac');
 console.log('in hotfix');
 console.log('in hotfix');
 console.log('in hotfix');
